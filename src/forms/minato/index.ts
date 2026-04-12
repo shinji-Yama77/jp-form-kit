f0f3109d99ac84f9213271d5626040ea3c691a31
shinji-Yama77 <shinchan396@gmail.com>
2026-04-12 17:23:51 +0800

    (cleanup) ran prettier to pass CI checks

--- src/forms/minato/index.ts
+++ src/forms/minato/index.ts
@@ -1,11 +1,16 @@
 export { juminhyoSchema } from "./juminhyo.js";
 export { teninSchema } from "./tenin.js";
 export { kosekiSchema } from "./koseki.js";
-export { juminhyoMailSchema } from "./juminhyo-mail.js"
+export { juminhyoMailSchema } from "./juminhyo-mail.js";
 
 import { juminhyoSchema } from "./juminhyo.js";
 import { teninSchema } from "./tenin.js";
 import { kosekiSchema } from "./koseki.js";
 import { juminhyoMailSchema } from "./juminhyo-mail.js";
 
-export const minatoForms = [juminhyoSchema, teninSchema, kosekiSchema, juminhyoMailSchema];
+export const minatoForms = [
+  juminhyoSchema,
+  teninSchema,
+  kosekiSchema,
+  juminhyoMailSchema,
+];
