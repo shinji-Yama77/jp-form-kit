4f77876373594fea1b3701e97a3e74a5438fb7b1
shinji-Yama77 <shinchan396@gmail.com>
2026-04-12 17:22:09 +0800

    (feat) added juminhyo-mail schema and updated some canonical keys

--- src/forms/minato/index.ts
+++ src/forms/minato/index.ts
@@ -1,9 +1,11 @@
 export { juminhyoSchema } from "./juminhyo.js";
 export { teninSchema } from "./tenin.js";
 export { kosekiSchema } from "./koseki.js";
+export { juminhyoMailSchema } from "./juminhyo-mail.js"
 
 import { juminhyoSchema } from "./juminhyo.js";
 import { teninSchema } from "./tenin.js";
 import { kosekiSchema } from "./koseki.js";
+import { juminhyoMailSchema } from "./juminhyo-mail.js";
 
-export const minatoForms = [juminhyoSchema, teninSchema, kosekiSchema];
+export const minatoForms = [juminhyoSchema, teninSchema, kosekiSchema, juminhyoMailSchema];
