90dc4da6c43edb7c074fe9a5fc60ac44af81da5e
shinji-Yama77 <shinchan396@gmail.com>
2026-04-12 15:08:37 +0800

    (chore) wired up koseki schema to the right jurisdiction minato-ku

--- src/forms/minato/index.ts
+++ src/forms/minato/index.ts
@@ -1,7 +1,9 @@
 export { juminhyoSchema } from "./juminhyo.js";
 export { teninSchema } from "./tenin.js";
+export { kosekiSchema } from "./koseki.js";
 
 import { juminhyoSchema } from "./juminhyo.js";
 import { teninSchema } from "./tenin.js";
+import { kosekiSchema } from "./koseki.js";
 
-export const minatoForms = [juminhyoSchema, teninSchema];
+export const minatoForms = [juminhyoSchema, teninSchema, kosekiSchema];
