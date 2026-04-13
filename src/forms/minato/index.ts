782adf6242fb713bd633c613f1cebb16c9af2343
shinji-Yama77 <shinchan396@gmail.com>
2026-04-13 23:06:49 +0900

    (feat) wired up inkan-shomei exports

--- src/forms/minato/index.ts
+++ src/forms/minato/index.ts
@@ -3,17 +3,20 @@ export { teninSchema } from "./tenin.js";
 export { kosekiSchema } from "./koseki.js";
 export { juminhyoMailSchema } from "./juminhyo-mail.js";
 export { mibunShomeishoSchema } from "./mibun-shomeisho.js";
+export { inkanShomeiSchema} from "./inkan-shomei.js"
 
 import { juminhyoSchema } from "./juminhyo.js";
 import { teninSchema } from "./tenin.js";
 import { kosekiSchema } from "./koseki.js";
 import { juminhyoMailSchema } from "./juminhyo-mail.js";
 import { mibunShomeishoSchema } from "./mibun-shomeisho.js";
+import { inkanShomeiSchema } from "./inkan-shomei.js";
 
 export const minatoForms = [
   juminhyoSchema,
   teninSchema,
   kosekiSchema,
-  juminhyoMailSchema,
+  juminhyoMailSchema, 
   mibunShomeishoSchema,
+  inkanShomeiSchema
 ];
