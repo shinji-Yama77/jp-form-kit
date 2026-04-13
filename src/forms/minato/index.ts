dcbc138ea5a5115d942c915b740533e702737d58
shinji-Yama77 <shinchan396@gmail.com>
2026-04-13 23:08:08 +0900

    (chore) ran prettier to fix CI issues

--- src/forms/minato/index.ts
+++ src/forms/minato/index.ts
@@ -3,7 +3,7 @@ export { teninSchema } from "./tenin.js";
 export { kosekiSchema } from "./koseki.js";
 export { juminhyoMailSchema } from "./juminhyo-mail.js";
 export { mibunShomeishoSchema } from "./mibun-shomeisho.js";
-export { inkanShomeiSchema} from "./inkan-shomei.js"
+export { inkanShomeiSchema } from "./inkan-shomei.js";
 
 import { juminhyoSchema } from "./juminhyo.js";
 import { teninSchema } from "./tenin.js";
@@ -16,7 +16,7 @@ export const minatoForms = [
   juminhyoSchema,
   teninSchema,
   kosekiSchema,
-  juminhyoMailSchema, 
+  juminhyoMailSchema,
   mibunShomeishoSchema,
-  inkanShomeiSchema
+  inkanShomeiSchema,
 ];
